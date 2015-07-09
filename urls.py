f0eac2ebe59b0d15aefd3adfc1e1a48c94c49be3
davidls <root@tseries.tseries.geodata.hawaii.edu>
2015-07-09 09:27:11 -1000

    fixed template to use the name portion of the url configuration.  Also made sure all urls end in /

--- urls.py
+++ urls.py
@@ -2,8 +2,8 @@ from . import views
 from django.conf.urls import url
 
 urlpatterns = [
-    url(r'^history$', views.user_history, name = 'user_history' ),
+    url(r'^history/$', views.user_history, name = 'user_history' ),
     url(r'^$', views.dashboard, name = 'user_dashboard'),
-    url(r'^getjobs/(?P<uid>\d+)$', views.print_jobs, name = 'print_jobs'),
-    url(r'^gettime/(?P<uid>\d+)$', views.print_time, name = 'print_time'),
+    url(r'^getjobs/(?P<uid>\d+)/$', views.print_jobs, name = 'print_jobs'),
+    url(r'^gettime/(?P<uid>\d+)/$', views.print_time, name = 'print_time'),
 ]
