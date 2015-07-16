bc4de3aaf79cb0f7657f1176cfb553a4e981d33d
Vivian Fang <vivian98.fang@gmail.com>
2015-07-15 15:31:52 -1000

    more tests

--- tests.py
+++ tests.py
@@ -3,7 +3,7 @@ from .views import *
 from .models import UohJobTable
 from .forms import UsernameForm
 import datetime
-
+import json
 import pseudopwd as pwd
 
 # Create your tests here.
@@ -15,8 +15,8 @@ class SetUpTestCase(TestCase):
         self.assertEqual(len(allJobs), 922)
 
     def test_get_uid(self):
-#        uid = pwd.getpwnam('root')[2]
-        allJobs = get_jobs(0)
+        uid = pwd.getpwnam('root')[2]
+        allJobs = get_jobs(uid)
         self.assertEqual(allJobs[0].id_user, 0)
 
 class HistoryTestCase(TestCase):
@@ -25,7 +25,33 @@ class HistoryTestCase(TestCase):
         form_data = {'username': 'root'}
         form = UsernameForm(data = form_data)
         self.assertEqual(form.is_valid(), True)
+        
     def test_get_username(self):
         form_data = {'username': 'root'}
         form = UsernameForm(data = form_data)
         self.assertEqual(get_username(form), 'root')
+
+class DashTestCase(TestCase):
+    fixtures = ['newout.json']
+    def test_print_views(self):
+        request = 'test request'
+        uid = 0
+        response = print_jobs(request, uid)
+        self.assertEqual(response.status_code, 200)
+        response = print_time(request, uid)
+        self.assertEqual(response.status_code, 200)
+
+    def test_json(self):
+        allJobs = get_jobs(0)
+        allJobs = change_times(allJobs)
+        jobs_list = json.loads(get_json_jobs(allJobs, 12))
+        self.assertEqual(len(jobs_list), 12)
+        time_list = json.loads(get_json_time(allJobs, 12))
+        self.assertEqual(len(jobs_list), 12)
+
+    def test_totals(self):
+        allJobs = get_jobs(0)
+        allJobs = change_times(allJobs)
+        total_jobs = tjobs(allJobs)
+        self.assertEqual(total_jobs[0], 0)
+        total_time = tcpuhours(allJobs)
